850f2b8bedaf01b7a7e7b0ac7016ca0912d2b459
Nguyen Anh Tuan <tuannguuyen21122000@gmail.com>
2022-09-18 15:05:12 +0700

    Pratical GraphQL

---
+++ Rookie.AssetManagement.DataAccessor/Data/Query.cs
@@ -0,0 +1,19 @@
+﻿using HotChocolate.Data;
+using Rookie.AssetManagement.DataAccessor.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rookie.AssetManagement.DataAccessor.Data
+{
+    public class Query
+    {
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<Brand> GetBrands => 
+            new List<Brand>().AsQueryable();
+    }
+}
---
+++ Rookie.AssetManagement.GraphQL/Program.cs
@@ -0,0 +1,33 @@
+using Rookie.AssetManagement.DataAccessor;
+using Rookie.AssetManagement.DataAccessor.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddDataAccessorLayer(builder.Configuration);
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddGraphQLServer().AddQueryType<Query>().AddProjections().AddFiltering().AddSorting();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapGraphQLHttp("/graphql");
+
+app.Run();
